dc54ae61636e3511f5deeaf906c54b898717064b
Shyam Patoliya <141806097+Shyam-Patoliya@users.noreply.github.com>
2025-04-06 19:31:45 +0530

    Update encoder_utils.py

--- models/llama3/multimodal/encoder_utils.py
+++ models/llama3/multimodal/encoder_utils.py
@@ -1,28 +1,13 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the terms described in the LICENSE file in
-# top-level folder for each specific model found within the models/ directory at
-# the top-level of this source tree.
-
-# Copyright (c) Meta Platforms, Inc. and its affiliates.
 import math
 from logging import getLogger
-
 import torch
 import torch.nn.functional as F
-
 from .utils import get_negative_inf_value, to_2tuple
 
 logger = getLogger()
 
 
 def resize_local_position_embedding(orig_pos_embed, grid_size):
-    """
-    Resize position embedding for vision encoder.
-    Original position embedding is [n_tiles * n_tiles + 1, dim]
-    New position embedding will be [grid_size[0] * grid_size[1] + 1, dim]
-    """
     new_grid_size = to_2tuple(grid_size)
     orig_grid_size = to_2tuple(int(math.sqrt(len(orig_pos_embed) - 1)))
     new_seq_len = new_grid_size[0] * new_grid_size[1] + 1
@@ -47,13 +32,6 @@ def resize_local_position_embedding(orig_pos_embed, grid_size):
 
 
 def initialize_global_position_embedding_from_local(pos_and_cls_embed, grid_size, x_scale, y_scale):
-    """
-    Takes a local position embedding for vision encoder and uses it
-    to initialize the global position embedding.
-    Input: local position embedding of shape [grid_size[0] * grid_size[1] + 1, dim]
-    Returns: global position embedding of shape [x_scale, y_scale, grid_size[0] * grid_size[1] + 1, dim]
-    Here x_scale and y_scale are the number of tiles along x-axis and y-axis respectively.
-    """
     pos_embed = pos_and_cls_embed[1:]
     cls_embed = pos_and_cls_embed[0].view(1, 1, 1, -1)
     grid_size = to_2tuple(grid_size)
@@ -75,26 +53,17 @@ def initialize_global_position_embedding_from_local(pos_and_cls_embed, grid_size
 
 
 def resize_global_position_embedding(pos_and_cls_embed, grid_size, x_scale, y_scale):
-    """
-    Takes a global position embedding for vision encoder and resizes it to new size.
-    Input: global position embedding of shape [x_old, y_old, old_grid_size[0] * old_grid_size[1] + 1, dim]
-    Returns: global position embedding of shape [x_scale, y_scale, grid_size[0] * grid_size[1] + 1, dim]
-    Here x_scale and y_scale are the number of tiles along x-axis and y-axis respectively.
-    """
-    # first remove cls token
     pos_embed = pos_and_cls_embed[:, :, 1:]
     cls_embed = pos_and_cls_embed[:, :, 0].unsqueeze(2)
 
     xs_old, ys_old, ntok, dim = pos_embed.shape
     old_grid_size = int(math.sqrt(ntok))
 
-    # move to correct form for interpolation
     pos_embed = pos_embed.view(xs_old, ys_old, old_grid_size, old_grid_size, dim)
     pos_embed = pos_embed.permute(0, 2, 1, 3, 4).contiguous()
     pos_embed = pos_embed.view(xs_old * old_grid_size, ys_old * old_grid_size, dim)
     pos_embed = pos_embed.unsqueeze(0)
 
-    # interpolate
     new_size = (grid_size[0] * x_scale, grid_size[1] * y_scale)
     pos_embed = pos_embed.permute(0, 3, 1, 2)
     pos_embed_resized = F.interpolate(
@@ -105,12 +74,10 @@ def resize_global_position_embedding(pos_and_cls_embed, grid_size, x_scale, y_sc
     )
     pos_embed = pos_embed_resized.permute(0, 2, 3, 1)[0]
 
-    # move it back in place
     pos_embed = pos_embed.view(x_scale, grid_size[0], y_scale, grid_size[1], dim)
     pos_embed = pos_embed.permute(0, 2, 1, 3, 4).contiguous()
     pos_embed = pos_embed.view(x_scale, y_scale, grid_size[0] * grid_size[1], dim)
 
-    # interpolate cls token
     cls_embed = cls_embed.permute(2, 3, 0, 1)
     cls_embed_resized = F.interpolate(
         cls_embed,
@@ -119,7 +86,7 @@ def resize_global_position_embedding(pos_and_cls_embed, grid_size, x_scale, y_sc
         align_corners=True,
     )
     cls_embed = cls_embed_resized.permute(2, 3, 0, 1)
-    # add cls token back in
+
     pos_and_cls_embed = torch.cat([cls_embed, pos_embed], dim=2)
 
     return pos_and_cls_embed
@@ -132,9 +99,7 @@ def build_encoder_attention_mask(
     num_chunks: int,
     n_heads: int,
 ):
-    """
-    Build vision encoder attention mask that omits padding tokens.
-    """
+
     masks = []
     for arx in ar:
         mask_i = torch.ones((num_chunks, x.shape[2], 1), dtype=x.dtype)
